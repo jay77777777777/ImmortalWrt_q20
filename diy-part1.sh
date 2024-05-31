75a4a150918c0ec64de1bb8a17d195381c2eb1df
jay77777777777 <142129784+jay77777777777@users.noreply.github.com>
2024-05-31 11:22:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/small-packages' feeds.conf.default
+sed -i '1i src-git kenzok https://github.com/kenzok8/small-package' feeds.conf.default
